c7c24cbf36f20d85d681b4fbaff432684b6994eb
skj <2427069776@qq.com>
2019-05-13 10:18:40 +0800

    ftp

---
+++ src/main/java/com/rm/example/demo/ftp/FtpUtil.java
@@ -0,0 +1,181 @@
+package com.rm.example.demo.ftp;
+
+
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+import org.apache.commons.net.ftp.FTPReply;
+import org.apache.log4j.Logger;
+
+import java.io.*;
+import java.net.SocketException;
+
+/**
+ * @Author: Song Kaijie
+ * @Date: 2019/5/7
+ */
+public class FtpUtil {
+    private static Logger log = Logger.getLogger(FtpUtil.class);
+    private static FTPClient ftpClient = null;
+    private String username;
+    private String password;
+    private String host;
+    private int port;
+
+    public FtpUtil(String username, String password, String host, int port) {
+        this.username = username;
+        this.password = password;
+        this.host = host;
+        this.port = port;
+    }
+
+    /**
+     * 初始化ftp服务器
+     */
+    public void initFtpClient() {
+        ftpClient = new FTPClient();
+        ftpClient.setControlEncoding("utf-8");
+        try {
+            ftpClient.connect(this.host, this.port);
+            ftpClient.login(this.username, this.password);
+            int replyCode = ftpClient.getReplyCode();
+            if (!FTPReply.isPositiveCompletion(replyCode)) {
+               log.info("ftp连接失败");
+            }
+            System.out.print("ftp连接成功");
+        } catch (SocketException e) {
+            log.error("异常");
+        } catch (IOException e) {
+            log.error("异常");
+        }
+    }
+
+    /**
+     * 上传文件
+     *
+     * @param pathname       ftp服务保存地址
+     * @param fileName       上传到ftp的文件名
+     * @param originfilename 待上传文件的名称（绝对地址）
+     * @return 是否成功
+     */
+    public boolean uploadFile(String pathname, String fileName, String originfilename) {
+        boolean flag = false;
+        InputStream inputStream = null;
+        try {
+           log.info("开始上传文件");
+            inputStream = new FileInputStream(new File(originfilename));
+            initFtpClient();
+            ftpClient.setFileType(FTPClient.BINARY_FILE_TYPE);
+            CreateDiecroty(pathname);
+            ftpClient.makeDirectory(pathname);
+            ftpClient.changeWorkingDirectory(pathname);
+            ftpClient.storeFile(fileName, inputStream);
+            inputStream.close();
+            ftpClient.logout();
+            flag = true;
+           log.info("上传文件成功");
+        } catch (FileNotFoundException e) {
+           log.info("上传文件失败");
+            log.error("异常");
+        } catch (IOException e) {
+            log.error("异常");
+        } finally {
+            if (ftpClient.isConnected()) {
+                try {
+                    ftpClient.disconnect();
+                } catch (IOException e) {
+                    log.error("异常");
+                }
+            }
+            if (null != inputStream) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    log.error("异常");
+                }
+            }
+        }
+        return flag;
+    }
+
+    //改变目录路径
+    private boolean changeWorkingDirectory(String directory) {
+        boolean flag = true;
+        try {
+            flag = ftpClient.changeWorkingDirectory(directory);
+            if (flag) {
+               log.info("进入文件夹" + directory + "成功");
+            } else {
+               log.info("进入文件夹" + directory + "失败");
+            }
+        } catch (IOException e) {
+            log.error("异常");
+        }
+        return flag;
+    }
+
+    //创建多层目录文件，如果有ftp服务器已存在该文件，则不创建，如果无，则创建
+    private boolean CreateDiecroty(String remote) throws IOException {
+        boolean success = true;
+        String directory = remote + "/";
+        //如果远程目录不存在，则递归创建远程服务器目录
+        if (!directory.equalsIgnoreCase("/") && !changeWorkingDirectory(new String(directory))) {
+            int start = 0;
+            int end = 0;
+            if (directory.startsWith("/")) {
+                start = 1;
+            } else {
+                start = 0;
+            }
+            end = directory.indexOf("/", start);
+            String path = "";
+            String paths = "";
+            while (true) {
+                String subDirectory = new String(remote.substring(start, end).getBytes("GBK"), "iso-8859-1");
+                path = path + "/" + subDirectory;
+                if (!existFile(path)) {
+                    if (makeDirectory(subDirectory)) {
+                        changeWorkingDirectory(subDirectory);
+                    } else {
+                       log.info("创建目录[" + subDirectory + "]失败");
+                        changeWorkingDirectory(subDirectory);
+                    }
+                } else {
+                    changeWorkingDirectory(subDirectory);
+                }
+                paths = paths + "/" + subDirectory;
+                start = end + 1;
+                end = directory.indexOf("/", start);
+                if (end <= start) {
+                    break;
+                }
+            }
+        }
+        return success;
+    }
+
+    //判断ftp服务器文件是否存在
+    private boolean existFile(String path) throws IOException {
+        boolean flag = false;
+        FTPFile[] ftpFileArr = ftpClient.listFiles(path);
+        if (ftpFileArr.length > 0) {
+            flag = true;
+        }
+        return flag;
+    }
+
+    private boolean makeDirectory(String dir) {
+        boolean flag = true;
+        try {
+            flag = ftpClient.makeDirectory(dir);
+            if (flag) {
+               log.info("创建文件夹" + dir + "成功");
+            } else {
+               log.info("创建文件夹" + dir + "失败！");
+            }
+        } catch (IOException e) {
+            log.error("异常");
+        }
+        return flag;
+    }
+
+}
---
+++ src/main/java/com/rm/example/demo/ftp/SFTPUtil.java
@@ -0,0 +1,8 @@
+package com.rm.example.demo.ftp;
+
+/**
+ * @Author: Song Kaijie
+ * @Date: 2019/5/13
+ */
+public class SFTPUtil {
+}
